a7f697ceb9027c2268e0d3a4aba717ca3879ac9a
NsptPl <78443359+NsptPl@users.noreply.github.com>
2023-11-03 01:29:05 -0700

    added search drop down option for the tags
    ** not tested yet due to fetch button not showing images

--- admin-web-client/data_display/tag-button.js
+++ admin-web-client/data_display/tag-button.js
@@ -6,6 +6,8 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import { Grid } from "@mui/material";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from '@mui/material/TextField';
 
 export default function TagSelection({ entry }) {
   const handleInput = (e) => {
@@ -13,27 +15,43 @@ export default function TagSelection({ entry }) {
 
     const index = cache.indexOf(entry);
     console.log(e.target.value);
-    cache[index].tag = e.target.value;
+    cache[index].Label = e.target.value;
   };
+
+  const labels_select = [
+    { label: 'Dog', value:"dog"},
+    { label: 'Cat', value:"cat"},
+    { label: 'Horse', value:"horse"},
+    { label: 'Neither', value:"either"}
+  ];
 
   return (
     <div>
       <img width={250} height={250} src={entry.imageUrl} />
       <Grid>
-      <label>Tag: </label>
+        <label>Tag: </label>
 
-        <FormControl variant="standard" sx={{ m: 1, minWidth: 120}}>
-          <Select
+        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+          <Autocomplete
+            disablePortal
+            id="tag_selection"
+            options={labels_select}
+            sx={{ width: 300 }}
+            renderInput={(params) => <TextField {...params} label="Label" />}
+          />
+
+          
+          {/* <Select
             name="new_tag"
-            defaultValue={entry.tag}
+            defaultValue={entry.Label}
             onChange={handleInput}
           >
             <MenuItem value="dog">Dog</MenuItem>
             <MenuItem value="cat">Cat</MenuItem>
-            <MenuItem value="none">Neither</MenuItem>
-          </Select>
+            <MenuItem value="None">Neither</MenuItem>
+          </Select> */}
         </FormControl>
-        </Grid>
+      </Grid>
     </div>
   );
 }
