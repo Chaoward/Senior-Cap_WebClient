148040cf130e7f80284a9c16d769ce1b7fd6dcfb
NsptPl <78443359+NsptPl@users.noreply.github.com>
2023-11-16 14:00:12 -0800

    Update data.js
    create theme to the page, change font and some color element

--- admin-web-client/data_display/data.js
+++ admin-web-client/data_display/data.js
@@ -1,72 +1,39 @@
-const cache = [
-    {
-        "imageUrl": "https://www.cdc.gov/healthypets/images/pets/cute-dog-headshot.jpg?_=42445",
-        "Label": "dog"
-    },
-    {
-        "imageUrl": "https://image.petmd.com/files/styles/978x550/public/dog-allergies.jpg",
-        "Label": "cat"
-    }
-]
+//import Image from 'next/image'
+"use client";
+import "./stylePage.css";
+import React from 'react';
+import ImageListData, { FetchButton, SendVerifiedButton } from "./display-data";
+import NavBar from "./NavBar";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { pink, purple } from "@mui/material/colors";
 
-//TODO: edit fetches to match new data format
+const theme = createTheme({
+  palette: {
+    primary: purple,
+    secondary: pink
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 600
+  }
+})
 
-async function fetchPending(callback) {
-    //GET request
-    await fetch("https://flaskapp.jondooley87.repl.co/getUnverifiedXs", {
-        method: "GET",
-        headers: { 'Access-Corntrol-Allow-Origin': 'http://localhost:3000', "Content-Type": "application/json" }
-    })
-        .then((res) => res.json()
-            .then((resJson) => {
-                //save to cache
-                resJson.forEach(ele => cache.push(ele));
-                callback(resJson);
-            }));
-    console.log(JSON.stringify(cache));
+export default function Home() {
+
+  return (
+  <ThemeProvider theme={theme}>
+    <html>
+      <head>
+      </head>
+      <div className="data-list" >
+        <NavBar/><br/>
+        <ImageListData/>
+      </div>
+    </html>
+  </ThemeProvider>
+  );
 }
 
-
-
-async function sendVerified(callback) {
-    //let alertString = cache.map((x) => `Image${cache.indexOf(x) + 1} : ${x.tag}\n`);
-    let response;
-
-    //POST request send verified data
-    try {
-        await fetch("https://flaskapp.jondooley87.repl.co/updateUnverified", {
-            method: "POST",
-            headers: { 'Access-Corntrol-Allow-Origin': 'http://localhost:3000', "Content-Type": "application/json" },
-            body: JSON.stringify(cache)
-        })
-        .then((res) => response = res);
-
-
-        await response.json().then(resJson => {
-            console.log(resJson);
-            
-            /*
-            if (!resJson.success) {
-                alert("POST request made but response was unsuccessful!");
-                alert(resJson);
-                return;
-            }*/
-        });
-
-        //clear cache
-        while (cache.length > 0)
-            cache.pop();
-
-        callback();
-    } catch (e) {
-        console.error(e);
-        alert(e);
-    }
-}
-
-
-module.exports = {
-    cache,
-    fetchPending,
-    sendVerified
-}
