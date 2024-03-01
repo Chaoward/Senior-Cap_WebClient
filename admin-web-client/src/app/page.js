4dcb483ee1f152047b9caa04e7955ff1cda78456
NsptPl <78443359+NsptPl@users.noreply.github.com>
2024-02-29 23:55:14 -0800

    Update page.js

--- admin-web-client/src/app/page.js
+++ admin-web-client/src/app/page.js
@@ -1,54 +1,38 @@
 "use client";
-import "./stylePage.css";
-import React, { useState, useRef  } from "react";
+import React, { useState } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import ImageListData from "./pages/display-data";
 import VersionHistory from "./pages/verison-control";
-import * as NavBar from "./components/NavBar";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { deepOrange, green, pink, purple } from "@mui/material/colors";
-
-
+import { VersionNavBar, VerifyNavBar } from "./components/NavBar";
+import "./stylePage.css";
 
 const theme = createTheme({
   palette: {
-    primary: {
-      main: "#5941a9",
-    },
-    secondary: {
-      main: "#6d72c3",
-    },
-    success: {
-      main: "#147b57",
-    },
-    info: {
-      main: "#2365a2",
-    },
-    error: {
-      main: "#a24575",
-    },
-  },
-  typography: {
-    fontFamily: "Quicksand",
-    fontWeightLight: 300,
-    fontWeightRegular: 400,
-    fontWeightMedium: 500,
-    fontWeightBold: 600,
+    primary: { main: "#5941a9" },
+    secondary: { main: "#6d72c3" },
+    success: { main: "#147b57" },
+    info: { main: "#2365a2" },
+    error: { main: "#a24575" },
   },
 });
 
 export default function Home() {
-  const [isVersionPage, setPage] = useState(false);
+  const [isVersionPage, setIsVersionPage] = useState(false);
 
-  const handleToggle = () => {
-    setPage(!isVersionPage);
+  const togglePage = () => {
+    setIsVersionPage(!isVersionPage);
   };
 
   return (
     <ThemeProvider theme={theme}>
       <div className="data-list">
-        {isVersionPage ? NavBar.VersionNavBar({onToggle: handleToggle}) : NavBar.VerifyNavBar({onToggle: handleToggle})}
+        {isVersionPage ? (
+          <VersionNavBar onToggle={togglePage} />
+        ) : (
+          <VerifyNavBar onToggle={togglePage} />
+        )}
         <br />
-        {isVersionPage ? <VersionHistory/> : <ImageListData/>}
+        {isVersionPage ? <VersionHistory /> : <ImageListData />}
       </div>
     </ThemeProvider>
   );
