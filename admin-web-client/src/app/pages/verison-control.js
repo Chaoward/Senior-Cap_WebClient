4168e8ede841a946093e41c0d34a7bbc0c0d4e25
NsptPl <78443359+NsptPl@users.noreply.github.com>
2024-03-01 00:01:28 -0800

    Update verison-control.js
    - added the sort drop-down to sort by ascending and descending 
    - the label showing the release version on the top left is now updating according to the select version and set version button 
    - added preview label function when clicking each version 
    - new fonts added, but not sure if it's going to show in the file. Might need to download it manually

--- admin-web-client/src/app/pages/verison-control.js
+++ admin-web-client/src/app/pages/verison-control.js
@@ -1,6 +1,4 @@
-import React, { useState } from "react";
-import {cache, fetchVersion, setRelease} from "../web-api/data";
-import "../components/ConfirmDialogAction";
+import React, { useState, useEffect } from "react";
 import {
   AppBar,
   Typography,
@@ -19,20 +17,40 @@ import {
   Button,
   Dialog,
   DialogTitle,
-  DialogContent
+  DialogContent,
+  DialogActions,
+  Chip,
 } from "@mui/material";
-import ConfirmButtonsDialog from "../components/ConfirmDialogAction";
 
 export default function VersionHistory() {
-  const [releaseDialog, setReleaseDialog] = useState(false);
-  const [deleteDialog, setDeleteDialog] = useState(false);
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [newLabels, setNewLabels] = useState([]);
+  const [rows, setRows] = useState([
+    {
+      version: "1.0.2",
+      date: "1-12-2024",
+      images: 3,
+      labels: "3",
+      size: "80MB",
+      newLabels: ["cow", "horse", "goat"],
+    },
+    {
+      version: "1.1.1",
+      date: "1-22-2024",
+      images: 2,
+      labels: "2",
+      size: "24MB",
+      newLabels: ["rabbit", "cat"],
+    },
+  ]);
 
-  const [selectedVer, setSelectedRows] = useState([]);
-  const [rows, setRows] = useState([...cache.versions]);
-  const [sortOrder, setSortOrder] = useState('newToOld');
+  const [sortOrder, setSortOrder] = useState("newToOld");
+  const [releaseVersion, setReleaseVersion] = useState(null);
+  const [selectedRow, setSelectedRow] = useState(null);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const versionNewLabels = selectedRow ? selectedRow.newLabels : [];
 
   const handleCheckboxChange = (version) => {
-    /*
     const selectedIndex = selectedRows.indexOf(version);
     let newSelected = [];
 
@@ -47,75 +65,90 @@ export default function VersionHistory() {
         selectedRows.slice(0, selectedIndex),
         selectedRows.slice(selectedIndex + 1)
       );
-    }*/
-    let i = selectedVer.indexOf(version);
-    if (i >= 0)
-      selectedVer.splice(i, 1);
-    else
-      selectedVer.push(version);
+    }
 
-    setSelectedRows([...selectedVer]);
+    setSelectedRows(newSelected);
   };
 
   const handleDeleteSelected = () => {
     // Filter out the selected rows
-    const updatedRows = rows.filter(row => !selectedVer.includes(row.version));
+    const updatedRows = rows.filter(
+      (row) => !selectedRows.includes(row.version)
+    );
     // Update the rows state
     setRows(updatedRows);
     // Clear the selected rows
     setSelectedRows([]);
   };
 
-
-  const handleRelease = () => {
-    if (selectedVer.length < 1) return;
-    setReleaseDialog(true);
+  const handleSetRelease = () => {
+    if (selectedRows.length === 1) {
+      const selectedRow = rows.find((row) => row.version === selectedRows[0]);
+      if (selectedRow) {
+        // Set the release version label
+        setReleaseVersion(selectedRow.version);
+      }
+    }
   };
-
-
-  const handleConfirm = () => {
-    console.log(selectedVer[0]);
-    setRelease(selectedVer[0]);
-  };
-
 
   const handleSortOrderChange = (event) => {
     setSortOrder(event.target.value);
   };
 
-
-  const handleFetch = () => {
-    fetchVersion((resJson) => {
-      setRows([...resJson.versions]);
-    });
+  const handleRowClick = (row) => {
+    setSelectedRow(row);
+    setDialogOpen(true);
   };
 
-  //const sortedRows = sortOrder === 'newToOld' ? rows.slice().reverse() : rows;
+  const handleCloseDialog = () => {
+    setDialogOpen(false);
+  };
 
+  const sortedRows =
+    sortOrder === "newToOld"
+      ? rows.slice().reverse()
+      : sortOrder === "asc"
+      ? [...rows].sort((a, b) => a.version.localeCompare(b.version))
+      : [...rows].sort((a, b) => b.version.localeCompare(a.version));
 
+  useEffect(() => {
+    // Extract labels from rows
+    const labels = rows.reduce((acc, row) => {
+      return acc.concat(row.newLabels);
+    }, []);
+    // Remove duplicates
+    const uniqueLabels = [...new Set(labels)];
+    setNewLabels(uniqueLabels);
+  }, [rows]);
 
   return (
     <>
-      {/* <Typography variant="h6" sx={{ mb: 2 }}>
-        Verification
-      </Typography> */}
-      <Toolbar sx={{ justifyContent: 'space-between' }}>
+      <Typography variant="h6">
+        Release: {releaseVersion ? releaseVersion : "None"}
+      </Typography>
+
+      <Toolbar sx={{ justifyContent: "space-between" }}>
         <FormControl>
           <Select value={sortOrder} onChange={handleSortOrderChange}>
             <MenuItem value="newToOld">New to Old</MenuItem>
-            <MenuItem value="oldToNew">Old to New</MenuItem>
+            <MenuItem value="asc">Ascending</MenuItem>
+            <MenuItem value="desc">Descending</MenuItem>
           </Select>
         </FormControl>
         <ButtonGroup>
-          <Button variant="contained" color="secondary" onClick={handleFetch}>
-            Fetch Versions
-          </Button>
-          <Button variant="contained" color="secondary" onClick={handleRelease}>
-            Set Release
-          </Button>
-          <div/>
-          <Button variant="contained" color="secondary" onClick={handleDeleteSelected}>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={handleDeleteSelected}
+          >
             Delete Selected
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSetRelease}
+          >
+            Set Release
           </Button>
         </ButtonGroup>
       </Toolbar>
@@ -126,56 +159,60 @@ export default function VersionHistory() {
               <TableCell>Version</TableCell>
               <TableCell>Date</TableCell>
               <TableCell>Images</TableCell>
-              <TableCell>New Labels</TableCell>
+              <TableCell>Labels</TableCell>
               <TableCell>Size</TableCell>
               <TableCell>Select</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => VersionEntry(row)) }
+            {sortedRows.map((row) => (
+              <TableRow key={row.version} onClick={() => handleRowClick(row)}>
+                <TableCell>{row.version}</TableCell>
+                <TableCell>{row.date}</TableCell>
+                <TableCell>{row.images}</TableCell>{" "}
+                <TableCell>{row.labels}</TableCell>
+                <TableCell>{row.size}</TableCell>
+                <TableCell>
+                  <Checkbox
+                    onChange={() => handleCheckboxChange(row.version)}
+                    checked={selectedRows.indexOf(row.version) !== -1}
+                  />
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
+
+      {/* Dialog for displaying preview images */}
+      <Dialog open={dialogOpen} onClose={handleCloseDialog}>
+        <DialogTitle>
+          Preview - Version {selectedRow?.version}
+        </DialogTitle>
+        <DialogContent>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <div style={{ marginBottom: "10px" }}>
+              <Typography variant="subtitle1">New label</Typography>
+              <div style={{ display: "flex", flexWrap: "wrap" }}>
+                {versionNewLabels.map((label, index) => (
+                  <Chip
+                    key={index}
+                    label={label}
+                    variant="outlined"
+                    style={{ marginRight: "10px", marginBottom: "10px" }}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
-
-
-
-  function VersionEntry(entry) {
-    const handleImagePreview = ()=> {
-  
-    };
-  
-    const handleLabelPreview = ()=> {
-      
-    };
-
-    //console.log(entry);
-  
-    return <>
-    <TableRow key={entry.version}>
-      <TableCell>{entry.version}</TableCell>
-      <TableCell>{new Date(entry.dateTrained).toISOString().split("T")[0]}</TableCell>
-      <TableCell>{entry.imagesTrained ? entry.imagesTrained.length : 0}</TableCell>
-      <TableCell>{entry.newLabels ? entry.newLabels.length : 0}</TableCell>
-      <TableCell>{entry.size}</TableCell>
-      <TableCell>
-        <Checkbox
-          onChange={() => handleCheckboxChange(entry.version)}
-          checked={selectedVer.indexOf(entry.version) !== -1}
-        />
-      </TableCell>
-    </TableRow>
-
-    {/* Set Release Dialog */}
-    <Dialog open={releaseDialog} onClose={() => setReleaseDialog(true)} fullScreen={false}>
-      <DialogContent>
-        <DialogTitle>
-          Set the Release Version to {selectedVer[0]}?
-        </DialogTitle>
-        {ConfirmButtonsDialog(handleConfirm, () => {setReleaseDialog(false)})}
-      </DialogContent>
-    </Dialog>
-    </>;
-  }
 }
