0debb362d0688837b12c0719845211b69a3d4397
NsptPl <78443359+NsptPl@users.noreply.github.com>
2024-03-29 00:47:29 -0700

    Reupload the file
    I think last push i accidentally put the wrong code in. This version is the right one.

--- admin-web-client/src/app/components/TrainModelBtn.js
+++ admin-web-client/src/app/components/TrainModelBtn.js
@@ -1,27 +1,17 @@
-"use client";
-import React, {useState} from "react";
-import { Button, } from "@mui/material";
-import { trainModel } from "../web-api/api";
-import ModelTrainingIcon from '@mui/icons-material/ModelTraining';
-
+import React from "react";
+import { Button } from "@mui/material";
+import LightbulbIcon from "@mui/icons-material/Lightbulb";
 
 export default function TrainModelBtn() {
-  const [isDisable, setDisable] = useState(false);
-
-  const handleClick = () => {
-    setDisable(true);
-    trainModel().then(res => {
-      if (!res.success) {
-        console.error(res.error);
-      }
-
-      setDisable(false);
-    });
-  };
-
   return (
-    <Button color="secondary" disable={isDisable} onClick={handleClick}>
-      <ModelTrainingIcon sx={{ mr: 1, color: "#1d1128" }} /> Train Model
+    <Button
+      startIcon={<LightbulbIcon style={{ color: "#1d1128" }} />}
+      color="secondary"
+      sx={{
+        mr: "1",
+      }}
+    >
+      Train Model
     </Button>
   );
 }
